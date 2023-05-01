16009a4a7b0b2c84c4fb430fb17575f5a970b9a5
Amar Prasad <amarprasad77354@gmail.com>
2023-05-01 21:43:38 +0530

    login&register in one

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-
+import jwt from "jsonwebtoken";
 
 
 // Database Part
 mongoose.connect("mongodb://127.0.0.1:27017", {
     dbName: "backend",
 })
-.then(() => console.log("Database Connected -:)"))
-.catch((error) => console.log(error))
+    .then(() => console.log("Database Connected -:)"))
+    .catch((error) => console.log(error))
 
-const userSchema = new mongoose.Schema({ 
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
- })
+})
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 const app = express();
 
@@ -30,29 +30,39 @@ app.use(cookieParser());
 // Setting up the view Engine
 app.set("view engine", "ejs");
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
-    if(token){
+    if (token) {
+        const decodedData = jwt.verify(token, "kd2bvu204n0vjw");
+        req.user = await User.findById(decodedData._id);
         next();
-    } else{
+    } else {
         res.render("login");
     }
 }
 
 app.get("/", isAuthenticated, (req, res) => {
-    res.render("logout");
+    res.render("logout", { name: req.user.name });
 });
 
-app.post("/login", (req, res) => {
-    res.cookie("token", "iaminboii",{
+app.post("/login", async (req, res) => {
+    const { name, email } = req.body;
+    const user = await User.create({
+        name, 
+        email,
+    })
+
+    // Making a josnWEBtoken so that we will not reveal cookie token to random users
+    const token = jwt.sign({_id: user._id}, "kd2bvu204n0vjw",);
+    res.cookie("token", token, {
         httpOnly: true,
-        expires: new Date(Date.now()+60*1000)
+        expires: new Date(Date.now() + 60 * 1000),
     });
     res.redirect("/");
 })
 
 app.get("/logout", (req, res) => {
-    res.cookie("token", null,{
+    res.cookie("token", null, {
         httpOnly: true,
         expires: new Date(Date.now())
     });
@@ -60,7 +70,7 @@ app.get("/logout", (req, res) => {
 })
 
 app.get("/add", async (req, res) => {
-    
+
     await Message.create({
         name: "Black Billa",
         email: "joshilaBilla@gmail.com"
